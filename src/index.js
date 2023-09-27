60538810d72f21279476dd7cf128bc8fe112630f
Rustam Gilmutdinov <rukavina_sb@mail.ru>
2023-09-27 23:52:42 +0500

    feat: task is preliminarily ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (bracketsConfig.includes(char)) {
+      stack.push(char);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let directKey = stack.at(-1)
+
+      if (bracketsConfig[char] === directKey) {
+        stack.pop()
+      } else {
+        return false
+      }
+    }
+  } return stack.length === 0;
 }
+
